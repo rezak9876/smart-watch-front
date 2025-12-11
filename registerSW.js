152af7ff8261151bcd2a9c3ddfd69f378f303dd7
rezak9876 <rezak9876.official@gmail.com>
2025-12-11 23:09:09 +0330

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/smart-watch-front/sw.js', { scope: '/smart-watch-front/' })})}
